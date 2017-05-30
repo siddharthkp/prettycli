934cc593393e64b19ff1daefe99a37d7bfba3cb0
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-05-30 18:16:36 +0530

    add silent option

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const chalk = require('chalk');
+const chalk = require('chalk')
 
 const colorMap = {
   info: 'green',
   warn: 'yellow',
   loading: 'blue',
   error: 'red'
-};
+}
 
 const print = (type, label, message) => {
-  const color = colorMap[type];
-  console.log(chalk.inverse.bold[color](` ${label} `), chalk[color](message), '\n');
-};
+  const color = colorMap[type]
+  console.log(
+    chalk.inverse.bold[color](` ${label} `),
+    chalk[color](message),
+    '\n'
+  )
+}
 
-const info = (label, message) => print('info', label, message);
-const warn = (message) => print('warn', 'WARN', message);
-const loading = (label, message) => print('loading', label, message);
+const info = (label, message) => print('info', label, message)
+const warn = message => print('warn', 'WARN', message)
+const loading = (label, message) => print('loading', label, message)
 
-const command = (command) => chalk.blue(command);
-const link = (url) => chalk.blue(url);
+const command = command => chalk.blue(command)
+const link = url => chalk.blue(url)
 
-const error = (err, options = {exit: true}) => {
-  print('error', 'ERROR', err);
-  if (options.exit) throw(new Error(err));
-};
+const error = (err, options = { exit: true, silent: false }) => {
+  print('error', 'ERROR', err)
+  if (options.silent) process.exit(1)
+  if (options.exit) throw new Error(err)
+}
 
-module.exports = {info, loading, warn, error, command, link};
+module.exports = { info, loading, warn, error, command, link }
