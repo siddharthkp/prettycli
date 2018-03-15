d58f464b11745c0dd22c404ac6334610e33c16f1
cg-cnu <sreenivas9alapati@gmail.com>
2018-03-15 12:34:30 +0530

    same set of refactoring for error function

--- index.js
+++ index.js
@@ -23,12 +23,10 @@ const loading = (message, label = 'LOADING') => print(message, label, 'loading')
 const command = message => chalk.blue(message)
 const link = command
 
-const error = (err, options) => {
-  const label = 'ERROR'
-  if (!options) options = { exit: true, silent: false, label }
-  print('error', options.label || label, err)
+const error = (message, label = 'ERROR', options = { exit: true, silent: false }) => {
+  print(message, label, 'error')
   if (options.silent) process.exit(1)
-  if (options.exit) throw new Error(err)
+  if (options.exit) throw new Error(message)
 }
 
 module.exports = { info, loading, warn, error, command, link }
